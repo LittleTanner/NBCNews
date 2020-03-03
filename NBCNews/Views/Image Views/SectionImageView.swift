bf28f1e2ed9149310849ec0f929d293272994423
Kevin Tanner <kevindavidtanner@gmail.com>
2020-03-02 17:07:47 -0800

    Cleans up code to remove constant that isn't being used anymore

--- NBCNews/Views/Image Views/SectionImageView.swift
+++ NBCNews/Views/Image Views/SectionImageView.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 class SectionImageView: UIImageView {
-    
-    let placeholderImage = UIImage(named: "nbcLogo")
 
     override init(frame: CGRect) {
         super.init(frame: frame)
